cfc7b236606fb995cfed91fe0798204a853e87d8
Cheng Li <daydreamerlee@gmail.com>
2016-05-09 23:14:55 -0700

    first commit HW7

---
+++ HW7/lcsh.c
@@ -0,0 +1,119 @@
+/* CSC 415 HW3  SHELL*/
+
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 1024
+#define ARGV_NUM_LIMIT 4
+#define READ_ERROR 1
+#define Empty_Input 2
+#define LARGER_THAN_BUFFER_INPUT 3
+#define DELIMITER " \t\n"
+
+
+void lcsh();
+int prompt_and_readinput(char*);
+int parse(char*,char**);
+
+int main(int argc, char const *argv[])
+{
+	lcsh();
+	return 0;
+}
+
+void lcsh(){
+	pid_t pid;
+	int process_status;
+	int input_status;
+	int parse_status;
+	char command_input[BUFFER_SIZE]; // User input Commands
+	char* commands[ARGV_NUM_LIMIT+1]; // parsed command and arguments
+
+	while(1)
+	{
+		/*Prompt_ And_ Input*/
+		input_status = prompt_and_readinput(command_input);
+
+		if (input_status == READ_ERROR)
+		{
+			fprintf(stderr, "Fail to read from stdin: %s\n", strerror(errno));
+			continue;
+		}
+
+		if (input_status == Empty_Input)
+		{
+			fprintf(stderr, "Empty Commands !\n");
+			continue;
+		} 
+		
+		/*Parse the arguments*/
+		parse_status = parse(command_input,commands);
+		printf("%d \n",parse_status); // for debug
+
+		if (parse_status > ARGV_NUM_LIMIT)
+		{
+			fprintf(stderr, "%d arguments, Too Many Arguments!\n", parse_status);
+			continue;
+		}
+
+		/*Execute commands*/
+		if ((strcmp((command_input),"quit")==0) || (strcmp((command_input),"exit")==0))
+		{
+			break;
+		}
+	
+		/*Fork and Exec*/	
+		pid = fork();
+		if(pid <0)
+		{
+			perror("Fork Failed:");
+			exit(1);
+		}
+
+		if(pid == 0)
+		{
+			if(execvp(commands[0],commands)!=0)
+				{
+					perror("Exec Failed");
+					exit(1);
+				}
+		}
+
+		wait(&process_status);
+	}
+	printf("Shell teminated!\n");
+	return;
+}
+
+int prompt_and_readinput(char* inputline){
+	printf("lcsh>> ");;
+	if(fgets(inputline,BUFFER_SIZE,stdin) == NULL){
+			return READ_ERROR;
+	}
+
+	if(isspace(*inputline)){
+		return Empty_Input;
+	}
+
+	return 0;
+}
+
+int parse(char* inputline, char ** parsed_cmds){
+	int args=0;
+	char* cmd;
+	cmd = strtok(inputline,DELIMITER);
+	while (cmd != NULL){
+		parsed_cmds[args] = cmd;
+		cmd = strtok (NULL,DELIMITER);
+		args++;
+	}
+	parsed_cmds[args] = cmd;
+	return args;
+}
---
+++ HW7/lcsh_2.c
@@ -0,0 +1,155 @@
+/* CSC 415 HW7  improved SHELL*/
+
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 1024
+#define ARGV_NUM_LIMIT 4
+#define READ_ERROR 1
+#define Empty_Input 2
+#define LARGER_THAN_BUFFER_INPUT 3
+#define DELIMITER " \t\n"
+
+
+void lcsh();
+int prompt_and_readinput(char*);
+int parse(char*,char**,int*);
+void copyArray (char**, char**, int ,int);
+
+int main(int argc, char const *argv[])
+{
+	lcsh();
+	return 0;
+}
+
+void lcsh(){
+	pid_t pid;
+	int process_status;
+	int input_status;
+	int total_index, special_index;
+	char command_input[BUFFER_SIZE]; // User input Commands
+	char* special_cmd;
+	char** commands = NULL; // parsed command and arguments
+	char** cmd_firstpart = NULL;
+	char** cmd_secondpart = NULL;
+
+	commands = malloc (1 * sizeof (char*));
+	while(1)
+	{
+		/*Prompt_ And_ Input*/
+		input_status = prompt_and_readinput(command_input);
+
+		if (input_status == READ_ERROR)
+		{
+			fprintf(stderr, "Fail to read from stdin: %s\n", strerror(errno));
+			continue;
+		}
+
+		if (input_status == Empty_Input)
+		{
+			fprintf(stderr, "Empty Commands !\n");
+			continue;
+		} 
+		
+		/*Parse the arguments*/
+		special_index = parse(command_input,commands, &total_index);
+		special_cmd = commands [special_index];
+		printf("%d %d %s\n",total_index, special_index,special_cmd); // for debug
+
+
+		/*Execute commands*/
+		if ((strcmp((command_input),"quit")==0) || (strcmp((command_input),"exit")==0))
+		{
+			break;
+		}
+	
+		/*Fork and Exec*/	
+		pid = fork();
+		if(pid <0)
+		{
+			perror("Fork Failed:");
+			exit(1);
+		}
+
+		if(pid == 0)
+		{
+			if(special_index!=0){
+				cmd_firstpart = malloc ((special_index) * sizeof (char*));
+				cmd_secondpart = malloc ((total_index - special_index) * sizeof (char*));
+			}
+			if(strcmp(special_cmd,"&")==0){
+
+
+			}else if(strcmp(special_cmd,">")==0){
+
+			}else if(strcmp(special_cmd,"<")==0){
+
+			}else if(strcmp(special_cmd,">>")==0){
+
+			}else if(strcmp(special_cmd,"|")==0){
+
+			}
+
+			if(execvp(commands[0],commands)!=0)
+				{
+					perror("Exec Failed");
+					exit(1);
+				}
+		}
+
+		wait(&process_status);
+	}
+	free (commands);	
+	printf("Shell teminated!\n");
+	return;
+}
+
+int prompt_and_readinput(char* inputline){
+	printf("lcsh>> ");;
+	if(fgets(inputline,BUFFER_SIZE,stdin) == NULL){
+			return READ_ERROR;
+	}
+
+	if(isspace(*inputline)){
+		return Empty_Input;
+	}
+
+	return 0;
+}
+
+int parse(char* inputline, char ** parsed_cmds, int* total_index){
+	int args=0;
+	int special_index=0;
+	char* cmd;
+	cmd = strtok(inputline,DELIMITER);
+	while (cmd != NULL){
+		parsed_cmds = realloc (parsed_cmds,(args+2) * sizeof(*parsed_cmds));
+		parsed_cmds[args] = cmd;
+		if(strcmp(cmd,">")==0 || strcmp(cmd,"<")==0 
+				|| strcmp(cmd,"|")==0 || strcmp(cmd,"&")==0
+				|| strcmp(cmd,">>")==0)
+		{
+			special_index = args;
+		}
+		cmd = strtok (NULL,DELIMITER);
+		args++;
+	}
+	parsed_cmds[args] = cmd;
+	*total_index = args;
+	return special_index;
+}
+
+void copyArray (char** input_array, char** output_array, int begin_index, int length){
+	int i;
+	for (i = 0; i < length; ++i)
+	{
+		output_array[i]= input_array[begin_index+i];
+	}
+}
